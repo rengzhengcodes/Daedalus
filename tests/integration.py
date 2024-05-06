eb3802466df3ffb2cd1017e992ead89f9a834c10
Renggeng Zheng <rzhengcontacts@gmail.com>
2024-05-06 03:25:38 -0400

    tests boiler

---
+++ tests/integration.py
@@ -0,0 +1,7 @@
+from ..architectures import Architecture
+from ..optimizers.sgd import SGD
+
+import os
+file_path = os.path.abspath(__file__)
+lib_path = os.path.join(os.path.dirname(file_path), "..", "lib")
+print(lib_path)
